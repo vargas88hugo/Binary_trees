9f6895d9939bcc7177cb6cb3e14a753538bbe4ab
hacheG <943@holbertonschool.com>
2019-12-03 10:35:13 -0500

    [FEATURE] they are leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,5 +7,17 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-
+    if (node == NULL)
+    {
+        return (0);
+    }
+    if (node->left != NULL || node->right != NULL)
+    {
+        return (0);
+    }
+    else
+    {
+        return (1);
+    }
+    
 }
