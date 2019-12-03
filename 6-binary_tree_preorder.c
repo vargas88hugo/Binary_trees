14f834bf9f15fc7210430fa78b5627098363424f
hacheG <943@holbertonschool.com>
2019-12-03 13:04:40 -0500

    [FEATURE] add pre-in-post_order_tasks6,7,8

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,5 +8,15 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (tree == NULL || func == NULL)
+    {
+        return;
+    }
+    else
+    {
+        func(tree->n);
+        binary_tree_preorder(tree->left, func);
+        binary_tree_preorder(tree->right, func);
+    }
+    
 }
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -8,5 +8,14 @@
  */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+    if (tree == NULL || func == NULL)
+    {
+        return;
+    }
+    else
+    {
+        binary_tree_inorder(tree->left, func);
+        func(tree->n);
+        binary_tree_inorder(tree->right, func);
+    }
 }
