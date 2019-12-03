fa23e684e244641ccb3b6edf820fda7fdc1f5cc2
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-03 14:57:29 -0500

    [feature] created 11-binary_tree_size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,5 +7,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	int left, right;
 
+	if (tree == NULL)
+		return (0);
+
+        left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);
+
+	return (left + 1 + right);
 }
