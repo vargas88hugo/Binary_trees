64ce5caee7f9b9a0d3ea19216e2c0970e9a2bdf0
hacheG <943@holbertonschool.com>
2019-12-03 14:24:04 -0500

    [FEATURE] count leaves_task12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,8 +7,21 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+    size_t leftLeaves, rightLeaves, leaves;
+
     if (tree == NULL)
     {
         return (0);
     }
+
+    if (tree->left == NULL && tree->right == NULL)
+    {
+        return (1);
+    }
+    leftLeaves = binary_tree_leaves(tree->left);
+    rightLeaves = binary_tree_leaves(tree->right);
+    leaves = leftLeaves + rightLeaves;
+
+    return (leaves);
+
 }
