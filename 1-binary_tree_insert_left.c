26b627ea3a4c23be9eee5a61d4ef255d49e12cde
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-04 14:56:33 -0500

    [fix] issue with case when the parent has node to the left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,6 +22,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new->left = NULL;
 	new->right = NULL;
 
+	if (parent->left != NULL)
+	{
+		new->left = parent->left;
+		new->left->parent = new;
+	}
 	parent->left = new;
 
 	return (new);
