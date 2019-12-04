f82c262a84afbc370f23f10b845c85e9c134a11e
hacheG <943@holbertonschool.com>
2019-12-04 16:51:15 -0500

    [FIX] put malloc validation

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,6 +10,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
 	newNode->n = value;
 	newNode->parent = parent;
 	newNode->left = NULL;
