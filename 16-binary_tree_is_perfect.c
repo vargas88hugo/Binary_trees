db6b9b35df010d1cdbccf4e05a732f4fcfe98be1
hacheG <943@holbertonschool.com>
2019-12-05 11:24:55 -0500

    [FEATURE] a perfect task&header

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,5 +7,61 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t depth;
 
+	if (tree == NULL)
+    {
+		return (0);
+    }
+    else
+    {
+	    depth = searchDown(tree);
+	    return (btPerfect(tree, depth, 0));
+    }
+    
+}
+/**
+ * btPerfect - Function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree
+ * @level: pointer to the root node of the tree
+ * Return: 0 if the tree is null or other positive number otherwise
+ */
+size_t btPerfect(const binary_tree_t *tree, int depth, int rank)
+{
+	if (tree == NULL)
+    {
+		return (1);
+    }
+    else
+    {
+	    if (tree->left == NULL && tree->right == NULL)
+        {
+		    return (depth == rank + 1);
+        }
+
+	    if (tree->left == NULL || tree->right == NULL)
+        {
+		    return (0);
+        }
+
+	    return (btPerfect(tree->left, depth, rank + 1) &&
+		    btPerfect(tree->right, depth, rank + 1));
+    }
+    
+}
+/**
+ * searchDown - Function that checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree
+ * Return: 0 if the tree is null or other positive number otherwise
+ */
+size_t searchDown(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree)
+	{
+		depth++;
+		tree = tree->left;
+	}
+	return (depth);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -46,6 +46,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t btHeight(const binary_tree_t *tree);
+size_t btPerfect(const binary_tree_t *tree, int depth, int level);
+size_t searchDown(const binary_tree_t *tree);
 
 void binary_tree_print(const binary_tree_t *);
 
