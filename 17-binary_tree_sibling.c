914e9e6c76979e2d8c070ebf66b60456c0f4fb78
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-06 11:21:42 -0500

    [feature] created 17-binary_tree_sibling.c

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,5 +7,13 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
+	if (node->parent->left == node)
+		return (node->parent->right);
+	if (node->parent->right == node)
+		return (node->parent->left);
+
+	return (NULL);
 }
