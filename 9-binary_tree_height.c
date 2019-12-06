9fd176fb7cd55b50f478208bf28006ab4d9e92a8
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-05 22:43:15 -0500

    [fix] one state was wrong

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -15,23 +15,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	if (tree)
-	{
-		if (tree->left)
-			left = 1 + binary_tree_height(tree->left);
-		else
-			left = 0;
-
-		if (tree->right)
-			right = 1 + binary_tree_height(tree->right);
-		else
-			right = 0;
-	}
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
 	else
-	{
-		if (left > right)
-			return (left);
-		else
-			return (right);
-	}
+		left = 0;
+
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	else
+		right = 0;
+
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
