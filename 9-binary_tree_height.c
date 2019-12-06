8c4ca479ffc405d76a53482490135adbd81588fb
Hugo Vargas <vargas88hugo@gmail.com>
2019-12-05 22:38:03 -0500

    [feature] created 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,7 +7,7 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left, right;
+	int left = 0, right = 0;
 
 	if (tree == NULL)
 		return (0);
@@ -17,13 +17,21 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		if (left > right)
-			return (left + 1);
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
 		else
-			return (right + 1);
+			left = 0;
+
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		else
+			right = 0;
 	}
 	else
 	{
-		return (0);
+		if (left > right)
+			return (left);
+		else
+			return (right);
 	}
 }
